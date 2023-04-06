2887bf79298ee57d3bdca67ac8f0f6f44c21c7ba
FernRicaurte <128169706+FernRicaurte@users.noreply.github.com>
2023-04-06 18:01:09 -0400

    matching/flipping w green bckgrnd

--- main.js
+++ main.js
@@ -16,16 +16,16 @@ function shuffle(array) {
 
 //make an array of card objects, did strings for value instead of numbers bc the kings/queens necessarily are not numbers.
 const cards = [
-    { value: '1', image: './images/diamonds_ace.svg' },
-    { value: '2', image: './images/clubs_2.svg' },
-    { value: '1', image: './images/diamonds_ace.svg' },
-    { value: '2', image: './images/clubs_2.svg' },
-    { value: '3', image: './images/clubs_3.svg' },
-    { value: '4', image: './images/spades_4.svg' },
-    { value: '3', image: './images/clubs_3.svg' },
-    { value: '5', image: './images/spades_5.svg' },
-    { value: '4', image: './images/spades_4.svg' },
-    { value: '5', image: './images/spades_5.svg' },
+    { value: '1', image: './images/diamonds_ace.svg', matched: false },
+    { value: '2', image: './images/clubs_2.svg', matched: false },
+    { value: '1', image: './images/diamonds_ace.svg', matched: false },
+    { value: '2', image: './images/clubs_2.svg', matched: false },
+    { value: '3', image: './images/clubs_3.svg', matched: false },
+    { value: '4', image: './images/spades_4.svg', matched: false },
+    { value: '3', image: './images/clubs_3.svg', matched: false },
+    { value: '5', image: './images/spades_5.svg', matched: false },
+    { value: '4', image: './images/spades_4.svg', matched: false },
+    { value: '5', image: './images/spades_5.svg', matched: false },
 ]
 
 // we can select all of the ten card elements by using .querySelectorAll and storing it in a variable cardEls. The selector is .playingCards which all cards have.
@@ -41,6 +41,12 @@ shuffle(cards)
 //adding an event listener for each of the ten cards in cardEls which is accessed with .querySelectorAll . the event listener is for a click on the cards.
 cardEls.forEach(function (el, index) {
      el.addEventListener('click', function () {
+      if (index === guessOne || cards[index].matched === true) {
+        alert('invalid guess')
+        return
+      }
+
+
         let cardClicked = cards[index]
   el.setAttribute('src', cardClicked.image) 
   //setAttribute method so facedown element being clicked on shows .image of card from array
@@ -49,6 +55,8 @@ cardEls.forEach(function (el, index) {
     guessOne = index
   } else {
     if (cards[guessOne].value === cards[index].value) { 
+      cards[guessOne].matched = true
+      cards[index] = true
       guessOne = null
     } else { 
       setTimeout(function () {
